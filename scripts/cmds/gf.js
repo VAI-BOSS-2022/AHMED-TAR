b2cb07e9c8acec1f97f878600d3fbfbb4aa789fe
MR-VAIYA-088 <mr.vaiya088@gmail.com>
2025-04-07 23:58:54 +0600

    Update gf.js

--- scripts/cmds/gf.js
+++ scripts/cmds/gf.js
@@ -1,38 +1,54 @@
-const axios = require('axios');
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = {
   config: {
     name: "gf",
-    version: "1.0.0",
-    author: "ASIF",
+    version: "1.4",
+    author: "MAHABUB RAHMAN",
     countDown: 5,
     role: 0,
-    description: {
-      en: "Find GF"
-    },
+    shortDescription: "Send GF pic with title",
+    longDescription: "Sends a GF image with message and author from API or keywords like 'gf de'",
     category: "LOVE",
-    guide: {
-      en: "{n}"
-    }
+    guide: "{pn}"
   },
 
-  onStart: async function() {},
-  onChat: async function({ api, event, message }) {
-    const input = event.body;
-    if (input && input.trim().toLowerCase().includes('gf de') || input && input.trim().toLowerCase().includes('bot gf de') || input && input.trim().toLowerCase().includes('need gf')) {
-      try {
-        api.setMessageReaction("⏳", event.messageID, (err) => {}, true);
-        const response = await axios.get('https://gf-imran.onrender.com/imugf');
-        const res = response.data.data;
-        api.setMessageReaction("✅", event.messageID, (err) => {}, true);
-        await message.reply({
-          body: `${res.title}`,
-          attachment: await global.utils.getStreamFromURL(res.url)
-        });
-      } catch (error) {
-        console.error('Error fetching data:', error.message);
-        message.reply('Error fetching data. Please try again later.');
-      }
+  onStart: async function ({ api, event }) {
+    return sendGf(api, event);
+  },
+
+  onChat: async function ({ event, api }) {
+    const message = event.body?.toLowerCase();
+    if (!message) return;
+
+    const triggerWords = ["gf", "gf de", "bot gf de","gf chai","give me gf"];
+    if (triggerWords.includes(message.trim())) {
+      return sendGf(api, event);
     }
   }
 };
+
+async function sendGf(api, event) {
+  try {
+    const res = await axios.get("https://gf-api-kie2.onrender.com/mahabubgf");
+    const { title, url } = res.data.data;
+    const authorName = res.data.author.name;
+
+    const fullMessage = `❥┈•${title}\n\nAuthor: ${authorName}....`;
+
+    const imgPath = path.join(__dirname, "cache", `gf.jpg`);
+    const imgRes = await axios.get(url, { responseType: "arraybuffer" });
+    fs.writeFileSync(imgPath, Buffer.from(imgRes.data, "binary"));
+
+    api.sendMessage({
+      body: fullMessage,
+      attachment: fs.createReadStream(imgPath)
+    }, event.threadID, () => fs.unlinkSync(imgPath), event.messageID);
+
+  } catch (err) {
+    console.error(err);
+    api.sendMessage("error fetching data.", event.threadID, event.messageID);
+  }
+}
