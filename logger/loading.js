f46afdb3a38e547f23dcf5f04b503189219dcf58
VAI-BOSS-2022 <bv903881@gmail.com>
2025-05-08 10:16:28 +0600

    Update loading.js

--- logger/loading.js
+++ logger/loading.js
@@ -1,14 +1,20 @@
 const { colors } = require('../func/colors.js');
 const moment = require("moment-timezone");
 const characters = '';
-const getCurrentTime = () => colors.gray(moment().tz('Asia/Ho_Chi_Minh').format('HH:mm:ss DD/MM/YYYY'));
+const getCurrentTime = () => colors.gray(moment().tz("Asia/Ho_Chi_Minh").format("HH:mm:ss DD/MM/YYYY"));
 
 function logError(prefix, message) {
 	if (message === undefined) {
 		message = prefix;
-		prefix = "ERROR";
+		prefix = "TARIF ERROR";
 	}
-	process.stderr.write(`\r${`${getCurrentTime()} ${colors.redBright(`${characters} ${prefix}:`)} ${message}`}`);
+	console.log(`${getCurrentTime()} ${colors.redBright(`${characters} ${prefix}:`)}`, message);
+	const error = Object.values(arguments).slice(2);
+	for (let err of error) {
+		if (typeof err == "object" && !err.stack)
+			err = JSON.stringify(err, null, 2);
+		console.log(`${getCurrentTime()} ${colors.redBright(`${characters} ${prefix}:`)}`, err);
+	}
 }
 
 module.exports = {
@@ -17,29 +23,42 @@ module.exports = {
 	warn: function (prefix, message) {
 		if (message === undefined) {
 			message = prefix;
-			prefix = "WARN";
+			prefix = "TARIF WARN";
 		}
-		process.stderr.write(`\r${`${getCurrentTime()} ${colors.yellowBright(`${characters} ${prefix}:`)} ${message}`}`);
+		console.log(`${getCurrentTime()} ${colors.yellowBright(`${characters} ${prefix}:`)}`, message);
 	},
 	info: function (prefix, message) {
 		if (message === undefined) {
 			message = prefix;
-			prefix = "INFO";
+			prefix = "TARIF INFO";
 		}
-		process.stderr.write(`\r${`${getCurrentTime()} ${colors.greenBright(`${characters} ${prefix}:`)} ${message}`}`);
+		console.log(`${getCurrentTime()} ${colors.greenBright(`${characters} ${prefix}:`)}`, message);
 	},
-	succes: function (prefix, message) {
+	success: function (prefix, message) {
 		if (message === undefined) {
 			message = prefix;
-			prefix = "SUCCES";
+			prefix = "TARIF SUCCES";
 		}
-		process.stderr.write(`\r${`${getCurrentTime()} ${colors.cyanBright(`${characters} ${prefix}:`)} ${message}`}`);
+		console.log(`${getCurrentTime()} ${colors.cyanBright(`${characters} ${prefix}:`)}`, message);
 	},
 	master: function (prefix, message) {
 		if (message === undefined) {
 			message = prefix;
-			prefix = "MASTER";
+			prefix = "TARIF MASTER";
 		}
-		process.stderr.write(`\r${`${getCurrentTime()} ${colors.hex("#eb6734", `${characters} ${prefix}:`)} ${message}`}`);
+		console.log(`${getCurrentTime()} ${colors.hex("#eb6734", `${characters} ${prefix}:`)}`, message);
+	},
+	dev: (...args) => {
+		if (["development", "production"].includes(process.env.NODE_ENV) == false)
+			return;
+		try {
+			throw new Error();
+		}
+		catch (err) {
+			const at = err.stack.split('\n')[2];
+			let position = at.slice(at.indexOf(process.cwd()) + process.cwd().length + 1);
+			position.endsWith(')') ? position = position.slice(0, -1) : null;
+			console.log(`\x1b[36m${position} =>\x1b[0m`, ...args);
+		}
 	}
 };
